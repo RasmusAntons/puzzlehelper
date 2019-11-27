d5c9e3ae9deadb8163bb8db4acb02c81635463e6
Rasmus Antons <mail@rasmusantons.de>
2019-11-27 04:18:29 +0100

    urlfinder.py: add itstoohard

--- lib/urlfinder.py
+++ lib/urlfinder.py
@@ -1,19 +1,27 @@
 import re
 import urllib.request
+import hashlib
+
+
+def check_itstoohard(u, r):
+    return hashlib.md5(r.read().split(b'</script>')[-1]).digest() != b'P\x9b\xaa2\x070n:\x89\x16\xfbB\xcd\x8dV\x1c'
+
 
 url_schemes = [
-    ('pastebin.com', 'https://pastebin.com/{}', r'^[a-zA-Z]{8}$', None),
-    ('youtube.com', 'https://www.youtube.com/watch?v={}', r'^[a-zA-Z0-9]{11}$', None),
-    ('bit.ly', 'https://bit.ly/{}', r'^[a-zA-Z0-9]{7}$', None),
-    ('imgur.com', 'https://imgur.com/a/{}', r'^[a-zA-Z0-9]{5,7}$', None),
-    ('imgur.com', 'https://imgur.com/{}', r'^[a-zA-Z0-9]{5,7}$', None),
-    ('bit.do', 'http://bit.do/{}', r'^[a-zA-Z0-9]+$', lambda u, r: u != r.geturl()),
-    ('mega.nz', 'https://mega.nz/{}', r'^#![a-zA-Z0-9]{8}![a-zA-Z0-9]{43}$', None)
+    ('pastebin.com', 'https://pastebin.com/{}', None, r'^[a-zA-Z]{8}$', None),
+    ('youtube.com', 'https://www.youtube.com/watch?v={}', None, r'^[a-zA-Z0-9]{11}$', None),
+    ('bit.ly', 'https://bit.ly/{}', None, r'^[a-zA-Z0-9]{7}$', None),
+    ('imgur.com', 'https://imgur.com/a/{}', None, r'^[a-zA-Z0-9]{5,7}$', None),
+    ('imgur.com', 'https://imgur.com/{}', None, r'^[a-zA-Z0-9]{5,7}$', None),
+    ('bit.do', 'http://bit.do/{}', None, r'^[a-zA-Z0-9]+$', lambda u, r: u != r.geturl()),
+    ('mega.nz', 'https://mega.nz/{}', None, r'^#![a-zA-Z0-9]{8}![a-zA-Z0-9]{43}$', None),
+    ('discord.gg', 'https://discordapp.com/api/v6/invites/{}', None, r'^[a-zA-Z0-9]+$', None),
+    ('itstoohard.com', 'http://www.itstoohard.com/puzzle/{}', None, r'^[a-zA-Z0-9]{8}$', check_itstoohard)
 ]
 
 
 def test_urls(s):
-    for label, frmt, pattern, validator in url_schemes:
+    for label, frmt, disp_frmt, pattern, validator in url_schemes:
         if re.match(pattern, s):
             url = frmt.format(s)
             try:
